41907cc49b624c6ebc0463853a0e8f4492f738b8
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-07 00:33:17 -0300

    chore: value type scanner

---
+++ Cae.DotNet.Framework.Loggers/NativeIOExtractionMode/Json/Reflections/ValueTypeScanner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cae.DotNet.Framework.Loggers.NativeIOExtractionMode.Json.Reflections
+{
+    public class ValueTypeScanner
+    {
+        public static bool IsSimpleValue(object value)
+        {
+            var type = value.GetType();
+
+            return (type.IsPrimitive || type.Namespace.StartsWith("System") || type.Namespace.StartsWith("Microsoft")) && !(value is IEnumerable);
+        }
+    }
+}
