1da3c797a85afe669ba446ec7ccd460822594975
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-06 23:19:47 -0300

    feat: logger contract

---
+++ Cae.DotNet.Framework.Loggers/ILogger.cs
@@ -0,0 +1,23 @@
+﻿namespace Cae.DotNet.Framework.Loggers
+{
+    public interface ILogger
+    {
+        /// <summary>
+        /// Method for loggin normal info
+        /// </summary>
+        /// <param name="info">Info the thex to log</param>
+        void LogInfo(string info);
+
+        /// <summary>
+        /// Method for loggin error info
+        /// </summary>
+        /// <param name="error">Error he text to log</param>
+        void LogError(string error);
+
+        /// <summary>
+        /// Method for loggin debug info
+        /// </summary>
+        /// <param name="info">Info the texto to log</param>
+        void LogDebug(string info);
+    }
+}
