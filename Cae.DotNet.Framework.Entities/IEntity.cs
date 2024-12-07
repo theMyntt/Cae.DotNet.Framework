bcaed6b1dbd76ed9c5ce956a4bde491901af2dc2
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-06 22:58:20 -0300

    feat: IEntity contract

---
+++ Cae.DotNet.Framework.Entities/IEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Cae.DotNet.Framework.Entities
+{
+    /**
+    * This is a marker interface. It is for marking classes which will be
+    * used as entities of your application. Not persistence entities,
+    * business entities.
+    */
+    public interface IEntity
+    {
+
+    }
+}
