9d4f4ae865d4809bed32db7d23f3d3b55ff7bb99
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-07 14:33:22 -0300

    chore: getter extractor

---
+++ Cae.DotNet.Framework.Loggers/NativeIOExtractionMode/Json/Reflections/GetterExtractor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cae.DotNet.Framework.Loggers.NativeIOExtractionMode.Json.Reflections
+{
+    public class GetterExtractor
+    {
+        private GetterExtractor() { }
+
+        public static IEnumerable<MethodInfo> ExecuteOn(object levelZero)
+        {
+            var type = levelZero.GetType();
+            var methods = type.IsEnum ? type.GetMethods(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static) : type.GetMethods();
+
+            return methods
+                .Where(m => m.Name.StartsWith("get") && m.Name != "GetType")
+                .ToList();
+        }
+    }
+}
