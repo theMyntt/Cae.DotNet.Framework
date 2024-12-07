7701a275d7ee7c0fb1c151a8938bca9c75aeac51
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-12-06 23:20:43 -0300

    feat: logger provider

---
+++ Cae.DotNet.Framework.Loggers/LoggerProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cae.DotNet.Framework.Loggers
+{
+    public class LoggerProvider
+    {
+        private LoggerProvider() { }
+
+        public static LoggerProvider Singleton { get; set; } = new LoggerProvider();
+
+        public ILogger? ProvidedInstance { get; set; }
+        public bool UseCasesLoggingIO { get; set; } = false;
+        public bool PortsLoggingIO { get; set; } = false;
+        public IOLoggingModeEnum IOLogginModeEnum { get; set; } = IOLoggingModeEnum.CaeNative;
+        public bool Async { get; set; } = false;
+        public bool StructuredFormat { get; set; } = false;
+        public bool LogStackTrace { get; set; } = false;
+        public int LinesOfStackTrace { get; set; } = 5;
+    }
+}
